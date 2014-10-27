24d624ed39f5ebdac2d7a504201c0d6dce00a846
Dave C <dave.campofthebell@gmail.com>
2014-10-28 03:47:49 +1000

    My first commit from BT

---
+++ Project02/src/pkg_02/HelloProj02.java
@@ -0,0 +1,13 @@
+package pkg_02;
+
+public class HelloProj02 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Project03/src/pkg_03/HelloProj03.java
@@ -0,0 +1,13 @@
+package pkg_03;
+
+public class HelloProj03 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
